5ef4bc8ad8a2aadbb5b4e6e3c366893ac1ea5392
Andranovia <yeromrenandra07@gmail.com>
2024-07-08 07:55:32 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Button from "./button";
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen w-full flex justify-center items-center">
+        <Button />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/button.tsx
@@ -0,0 +1,6 @@
+
+export default function Button() {
+  return (
+    <button className="px-8 py-4 bg-blue-300 rounded-md text-white">Click Me</button>
+  )
+}
